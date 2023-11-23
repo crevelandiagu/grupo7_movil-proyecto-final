afb63baa40f7159193cdb723fa23b5899b00cd93
dhuertasm <dhuertasm@uniandes.edu.co>
2023-11-22 20:00:12 -0500

    done

---
+++ lib/candidates/views/test_results.dart
@@ -0,0 +1,61 @@
+import 'package:abc_jobs/candidates/services/candidate_performance_service.dart';
+import 'package:abc_jobs/common_widgets/widgets.dart';
+import 'package:abc_jobs/utils/test_result_candidate_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TestResults extends StatelessWidget {
+  TestResults({super.key, required this.service});
+
+  CandidatePerformanceService service;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(),
+      bottomNavigationBar: bottomNavigation((index) => null, context, 2),
+      body: FutureBuilder<List<dynamic>>(
+        future: service.getAllTestResults(),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          var data = snapshot.data!;
+
+          var testResults = TestResultCandidate(data: data, context: context);
+
+          return Column(
+            children: [
+              const SizedBox(
+                height: 15,
+              ),
+              PaginatedDataTable(
+                header: Center(
+                    child: Text(AppLocalizations.of(context)!.testResults,
+                        style: GoogleFonts.workSans(
+                            fontSize: 20, fontWeight: FontWeight.w500))),
+                columns: [
+                  DataColumn(
+                      label: Text(AppLocalizations.of(context)!.project)),
+                  DataColumn(
+                      label: Text(AppLocalizations.of(context)!.company)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.test)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.score)),
+                  DataColumn(
+                      label: Text(AppLocalizations.of(context)!.comments)),
+                ],
+                source: testResults,
+                rowsPerPage: 8,
+                horizontalMargin: 15,
+                columnSpacing: 70,
+                showCheckboxColumn: false,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
