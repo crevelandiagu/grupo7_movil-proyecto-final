0798f4b652aa249daad6ba09406b5097e56847a9
dhuertasm <dhuertasm@uniandes.edu.co>
2023-11-26 21:49:20 -0500

    commit

--- test/list_interviews_test.dart
+++ test/list_interviews_test.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:abc_jobs/candidates/services/interview_service.dart';
 import 'package:abc_jobs/candidates/views/list_interviews.dart';
 import 'package:abc_jobs/utils/test_util.dart';
+import 'package:abc_jobs/utils/util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
@@ -11,6 +14,8 @@ import 'package:http/http.dart' as http;
 
 import 'auth_service_test.mocks.dart';
 import 'list_interviews_test.mocks.dart';
+
+//class Client extends Mock implements http.Client {}
 
 @GenerateMocks([InterviewService])
 void main() {
@@ -146,6 +151,9 @@ void main() {
 
   testWidgets("test list intervies view", (WidgetTester tester) async {
     FlutterError.onError = ignoreOverflowErrors;
+
+    // when(client.get(Uri.parse(""), headers: buildHeaders()))
+    //     .thenAnswer((_) async => http.Response('[]', 200));
 
     when(service.getInterviewsCanditate()).thenAnswer((_) => mockData());
 
